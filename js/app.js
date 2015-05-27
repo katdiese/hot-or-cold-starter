4f95d4fd65a6451b1b624b636cf070ed5372ffa8
Kathy Dieser <katdiese@gmail.com>
2015-05-26 22:11:13 -0600

    refactor, clean up

--- js/app.js
+++ js/app.js
@@ -19,74 +19,71 @@ $(document).ready(function(){
  $(".button").click(function(event) {
    event.preventDefault();
    var guess = $(".text").val();
-//   count++;
-    
    
-  if( guess == correctAnswer) {
-      alert("YOU'VE LITERALLY CAUGHT ON FIRE!!! SOMEONE GET A FIRE EXTINGUISHER!!!(you win)");
-      location.reload(false);
-    }
-  else if(guess <= correctAnswer - 46) 
-    {
-        $("#feedback").html("<h3>Colder than a Polar Bear's backside</h3>");
-    }
-  else if(guess >= correctAnswer + 46) 
-    {
-      $("#feedback").html("<h3>Colder than a Polar Bear's backside</h3>");
-    }
-  else if(guess <= correctAnswer - 21 && guess >= correctAnswer - 45) 
-    {
-      $("#feedback").html("<h3>Cold!</h3>");
-    }
-  else if(guess >= correctAnswer + 21 && guess <= correctAnswer + 45)
-    {
-      $("#feedback").html("<h3>Cold!</h3>");
-    }
-  else if (guess <= correctAnswer - 10 && guess >= correctAnswer -20) 
-    {
-      $("#feedback").html("<h3>Cold-ish</h3>");        
-    }
-  else if (guess >= correctAnswer + 10 && guess <= correctAnswer + 20)
-    {
-      $("#feedback").html("<h3>Cold-ish</h3>");
-    }
-  else if(guess <= correctAnswer - 5 && guess >= correctAnswer - 9) 
-    {
-      $("#feedback").html("<h3>Pretty Warm</h3>");
-    }
-  else if(guess >= correctAnswer + 5 && guess <= correctAnswer + 9)
-    {
-      $("#feedback").html("<h3>Pretty Warm</h3>");
-    }
-  else if(guess <= correctAnswer - 2 && guess >= correctAnswer - 4) 
-    {
-      $("#feedback").html("<h3>Burning up!!</h3>");
-    }
-  else if(guess >= correctAnswer + 2 && guess <= correctAnswer + 4)
-    { 
-      $("#feedback").html("<h3>Burning up!!</h3>");
-    }
-  else if(guess <= correctAnswer - 1)
-    {
-      $("#feedback").html("<h3>YOU'RE ON FIRE!!!</h3>");
-    }
-  else if(guess >= correctAnswer + 1)
-    {
-      $("#feedback").html("<h3>YOU'RE ON FIRE!!!</h3>");
-    }
-   
-  if(guess.length >= 1 && guess <= 100)
+   if(guess.length >= 1 && guess <= 100)
     {
       count++;
       $("#count").html(count);
       $("#guessList").append("<li>" + guess + "</li>");
       $(".text").val("");    
     }
-    else 
+   else 
     {
-//      count--;
-//      $("#count").html(count);
-      alert("Please enter a number between 1 and 100");  
+      alert("Please enter a number between 1 and 100");
+      return;
+    }
+   
+   if( guess == correctAnswer) {
+      alert("YOU'VE LITERALLY CAUGHT ON FIRE!!! SOMEONE GET A FIRE EXTINGUISHER!!!(you win)");
+      location.reload(false);
+    }
+   else if(guess <= correctAnswer - 46) 
+    {
+        $("#feedback").html("<h3>Colder than a Polar Bear's backside</h3>");
+    }
+   else if(guess >= correctAnswer + 46) 
+    {
+      $("#feedback").html("<h3>Colder than a Polar Bear's backside</h3>");
+    }
+   else if(guess <= correctAnswer - 21 && guess >= correctAnswer - 45) 
+    {
+      $("#feedback").html("<h3>Cold!</h3>");
+    }
+   else if(guess >= correctAnswer + 21 && guess <= correctAnswer + 45)
+    {
+      $("#feedback").html("<h3>Cold!</h3>");
+    }
+   else if (guess <= correctAnswer - 10 && guess >= correctAnswer -20) 
+    {
+      $("#feedback").html("<h3>Cold-ish</h3>");        
+    }
+   else if (guess >= correctAnswer + 10 && guess <= correctAnswer + 20)
+    {
+      $("#feedback").html("<h3>Cold-ish</h3>");
+    }
+   else if(guess <= correctAnswer - 5 && guess >= correctAnswer - 9) 
+    {
+      $("#feedback").html("<h3>Pretty Warm</h3>");
+    }
+   else if(guess >= correctAnswer + 5 && guess <= correctAnswer + 9)
+    {
+      $("#feedback").html("<h3>Pretty Warm</h3>");
+    }
+   else if(guess <= correctAnswer - 2 && guess >= correctAnswer - 4) 
+    {
+      $("#feedback").html("<h3>Burning up!!</h3>");
+    }
+   else if(guess >= correctAnswer + 2 && guess <= correctAnswer + 4)
+    { 
+      $("#feedback").html("<h3>Burning up!!</h3>");
+    }
+   else if(guess <= correctAnswer - 1)
+    {
+      $("#feedback").html("<h3>YOU'RE ON FIRE!!!</h3>");
+    }
+   else if(guess >= correctAnswer + 1)
+    {
+      $("#feedback").html("<h3>YOU'RE ON FIRE!!!</h3>");
     }
     
   });
@@ -98,4 +95,3 @@ $(document).ready(function(){
   
 
 });
-
